5d143f975bf788b55d59ca864d4a6fce3f9e02e5
jaccobrouwer <63238094+jaccobrouwer@users.noreply.github.com>
2020-10-01 10:25:28 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,7 +49,7 @@ const IndexPage = () => (
       </Section>
 
       <Section textAlign="center">
-        <iframe title="Video met introductie van Sigalen" src="https://www.youtube-nocookie.com/embed/HRPUDvkn57Y?autoplay=0&showinfo=0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe title="Video met introductie van Sigalen" src="https://www.youtube-nocookie.com/embed/AdBNyYwbf8A?autoplay=0&showinfo=0" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </Section>
 
       <Section>
