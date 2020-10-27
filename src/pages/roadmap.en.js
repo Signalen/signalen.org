748798736475aa3fb9698c86e4200c9a62c232d9
Felix Faassen <felix.faassen@protonmail.com>
2020-10-27 12:00:53 +0100

    Add English translation to roadmap component

--- src/pages/roadmap.en.js
+++ src/pages/roadmap.en.js
@@ -54,26 +54,26 @@ const RoadmapPage = ({ location }) => (
         </Timeline.Container>
         <Timeline.Container align="right">
           <Timeline.Content>
-            <Span fontSize="0.9rem">2 maart 2020</Span>
-            <Heading as="h3" fontSize="1.5rem">Kopgroep Signalen</Heading>
-            <p>‘s-Hertogenbosch, Amsterdam, Almere, VNG Realisatie en de Foundation for Public Code vormen samen de Kopgroep met als doel om Signalen bruikbaar te maken voor andere gemeenten.</p>
+            <Span fontSize="0.9rem">March 2nd, 2020</Span>
+            <Heading as="h3" fontSize="1.5rem">Signalen core group</Heading>
+            <p>'s-Hertogenbosch, Amsterdam, Almere, VNG Realization and the Foundation for Public Code together form the Kopgroep with the aim of making Signalen usable for other municipalities.</p>
             <ResponsiveImage src={connectingTeams} />
           </Timeline.Content>
         </Timeline.Container>
         <Timeline.Container align="left">
           <Timeline.Content>
             <Span fontSize="0.9rem">April 2020</Span>
-            <Heading as="h3" fontSize="1.5rem">Kopgroep van start</Heading>
-            <p>De Kopgroep start met het aanpassen van de code in sprints van twee weken.</p>
-            <p>De Signalen-software die in Amsterdam is ontwikkeld bevatte allerlei specifieke Amsterdam variabelen waardoor de Signalen software niet onmiddellijk bruikbaar was voor andere gemeenten en overheden.</p>
+            <Heading as="h3" fontSize="1.5rem">Core group starts</Heading>
+            <p>The core group will start adapting the code in sprints of two weeks.</p>
+            <p>The Signalen software that was developed in Amsterdam contained all kinds of specific Amsterdam variables which made the Signalen software not immediately usable for other municipalities and governments.</p>
             <ResponsiveImage src={sprint} />
           </Timeline.Content>
         </Timeline.Container>
         <Timeline.Container align="right">
           <Timeline.Content>
             <Span fontSize="0.9rem">Aug 2020</Span>
-            <Heading as="h3" fontSize="1.5rem">Eerste communitydag</Heading>
-            <p>Eerste Signalen Communitydag met de Kopgroep. Ook de gemeenten Den Haag, Utrecht en Haarlem zijn aanwezig als Signalen supporters.</p>
+            <Heading as="h3" fontSize="1.5rem">First community day</Heading>
+            <p>First Signalen Community day with the core group. The municipalities of The Hague, Utrecht and Haarlem are also present as Signalen supporters.</p>
             <ResponsiveImage src={community} />
           </Timeline.Content>
         </Timeline.Container>
@@ -81,29 +81,29 @@ const RoadmapPage = ({ location }) => (
           <Timeline.Content>
             <Span fontSize="0.9rem">Sept 2020</Span>
             <Heading as="h3" fontSize="1.5rem">Signalen as a Service</Heading>
-            <p>Uitwerken van de gemeentelijke eisen en wensen v.w.b. Beheer en ondersteuning op ‘Signalen-as-a-Service’.</p>
+            <p>Elaboration of the municipal requirements and wishes for management and support on 'Signalen-as-a-Service'.</p>
             <ResponsiveImage src={saas} />
           </Timeline.Content>
         </Timeline.Container>
         <Timeline.Container align="right">
           <Timeline.Content>
             <Span fontSize="0.9rem">Sept 2020</Span>
-            <Heading as="h3" fontSize="1.5rem">Integrale roadmap </Heading>
-            <p>De start richting één integrale functionele roadmap. De roadmap laat zien waar Signalen heen gaat en bevat features die voor iedere gemeente waardevol zijn.</p>
+            <Heading as="h3" fontSize="1.5rem">Integral roadmap</Heading>
+            <p>The start towards one integral functional roadmap. The roadmap shows where Signalen is going and contains features that are valuable for every municipality.</p>
           </Timeline.Content>
         </Timeline.Container>
         <Timeline.Container align="left">
           <Timeline.Content>
             <Span fontSize="0.9rem">Q4 2020</Span>
-            <Heading as="h3" fontSize="1.5rem">Klaar voor productie</Heading>
-            <p>Oplevering gemeente-neutrale versie Signalen-software. Signalen is klaar voor productie bij de gemeentelijke deelnemers uit de Kopgroep.</p>
+            <Heading as="h3" fontSize="1.5rem">Ready for production</Heading>
+            <p>Delivery of a municipal neutral version of the Signalen software. Signalen is ready for production for the municipal participants from the core group.</p>
           </Timeline.Content>
         </Timeline.Container>
         <Timeline.Container align="right">
           <Timeline.Content>
             <Span fontSize="0.9rem">2021</Span>
-            <Heading as="h3" fontSize="1.5rem">Opschalen</Heading>
-            <p>Opschalen van Signalen bij Nederlandse gemeenten.</p>
+            <Heading as="h3" fontSize="1.5rem">Scaling up</Heading>
+            <p>Scaling up Signalen at Dutch municipalities.</p>
             <ResponsiveImage src={doorontwikkeling} />
           </Timeline.Content>
         </Timeline.Container>
