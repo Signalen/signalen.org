b2a947d4688e807ad5011a708200e16194e06add
Felix Faassen <felix@publiccode.net>
2021-02-11 12:42:49 +0100

    add GraphQL to filter language specific posts

--- src/pages/blog/index.en.js
+++ src/pages/blog/index.en.js
@@ -5,17 +5,10 @@ import { graphql } from "gatsby"
 import SEO from '../../components/SEO'
 import Navigation from '../../components/Navigation'
 import Layout from '../../components/Layout'
-import Flex from '../../components/Flex'
-import Box from '../../components/Box'
-import ResponsiveImage from '../../components/ResponsiveImage'
+
+
 import Container from '../../components/Container'
 import Footer from '../../components/Footer'
-import Section from '../../components/Section'
-import Heading from '../../components/Heading'
-import Span from '../../components/Span'
-import Background from '../../components/Background'
-import Logos from '../../components/Logos'
-
 
 import PostLink from "../../components/PostLink"
 
@@ -29,44 +22,37 @@ const IndexPage = ({
 }) => (
   <Layout>
     <SEO title="Signalen - Latest news" />
-    <Background backgroundColor="#A8D0E6">
-      <Container>
-        <Navigation as="nav" location={location} />
-      </Container>
-    </Background>
+          <Container>
+            <Navigation as="nav" location={location} />
+          </Container>
 
-    <Container>
-
-        <Flex>
-          <Box>
-          <h2>Latest news</h2>
-          <p>{
-            edges.filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-              .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
-
-          }</p>
-
-          </Box>
-        </Flex>
-
-    </Container>
-    <Footer />
-  </Layout>
+        <Container>
+              <h2>Latest news</h2>
+              <p>{
+                edges.filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+                  .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+              }</p>
+        </Container>
+          <Footer />
+      </Layout>
 )
 
 export default IndexPage
 
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] } filter: {
+      frontmatter: { lang: { eq: "en" } }
+    }) {
       edges {
         node {
           id
           excerpt(pruneLength: 250)
           frontmatter {
-            date(formatString: "MMMM DD, YYYY")
+            date(formatString: "MMMM DD, YYYY", locale: "en")
             path
             title
+            lang
           }
         }
       }
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -5,16 +5,9 @@ import { graphql } from "gatsby"
 import SEO from '../../components/SEO'
 import Navigation from '../../components/Navigation'
 import Layout from '../../components/Layout'
-import Flex from '../../components/Flex'
-import Box from '../../components/Box'
-import ResponsiveImage from '../../components/ResponsiveImage'
 import Container from '../../components/Container'
 import Footer from '../../components/Footer'
-import Section from '../../components/Section'
-import Heading from '../../components/Heading'
-import Span from '../../components/Span'
-import Background from '../../components/Background'
-import Logos from '../../components/Logos'
+
 
 
 import PostLink from "../../components/PostLink"
@@ -33,21 +26,13 @@ const IndexPage = ({
       <Container>
         <Navigation as="nav" location={location} />
       </Container>
-  
-    <Container>
 
-        <Flex>
-          <Box>
+    <Container>
           <h2>Laatste nieuws</h2>
           <p>{
             edges.filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
               .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
-
           }</p>
-
-          </Box>
-        </Flex>
-
     </Container>
     <Footer />
   </Layout>
@@ -56,19 +41,22 @@ const IndexPage = ({
 export default IndexPage
 
 export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            path
-            title
-          }
+query {
+  allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] } filter: {
+    frontmatter: { lang: { eq: "nl" } }
+  }) {
+    edges {
+      node {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          date(formatString: "DD MMMM YYYY", locale: "nl")
+          path
+          title
+          lang
         }
       }
     }
   }
+}
 `
