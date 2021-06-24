9ea4ee3d449ca368c0d59eef416b3d02276cf010
Felix Faassen <felix@publiccode.net>
2021-06-24 16:26:53 +0200

    change order in roadmap latest -> old EN

--- src/pages/roadmap.en.js
+++ src/pages/roadmap.en.js
@@ -42,30 +42,58 @@ const RoadmapPage = ({ location }) => (
     </Container>
     <Container>
       <Timeline>
-      <Timeline.Container align="right">
-          <Timeline.Content>
-            <Span fontSize="0.9rem">2018</Span>
-            <Heading as="h3" fontSize="1.5rem">Start of development of SIA</Heading>
-            <p>The Municipality of Amsterdam starts the development of Signalen in Amsterdam (SIA) with the Department of Services and the Datalab. SIA is open source and therefore freely usable for others.</p>
-            <ResponsiveImage src={startOntwikkeling} />
-          </Timeline.Content>
-        </Timeline.Container>
+
         <Timeline.Container align="left">
           <Timeline.Content>
-            <Span fontSize="0.9rem">2020</Span>
-            <Heading as="h3" fontSize="1.5rem">Start initiative</Heading>
-            <p>Start of the Signalen initiative! Many municipalities in the Netherlands are interested in Signalen. Based on this enthusiasm, VNGr starts scaling up Signalen.</p>
-            <ResponsiveImage src={startInitiatief} />
+            <Span fontSize="0.9rem">May 2021</Span>
+            <Heading as="h3" fontSize="1.5rem">Signalen is used in production at the municipality of 's-Hertogenbosch</Heading>
+            <p>Signalen was taken into production at the beginning of May in the municipality of 's-Hertogenbosch.</p>
           </Timeline.Content>
         </Timeline.Container>
+
         <Timeline.Container align="right">
           <Timeline.Content>
-            <Span fontSize="0.9rem">March 2nd, 2020</Span>
-            <Heading as="h3" fontSize="1.5rem">Signalen core group</Heading>
-            <p>'s-Hertogenbosch, Amsterdam, Almere, VNG Realization and the Foundation for Public Code together form the Kopgroep with the aim of making Signalen usable for other municipalities.</p>
-            <ResponsiveImage src={connectingTeams} />
+            <Span fontSize="0.9rem">2021</Span>
+            <Heading as="h3" fontSize="1.5rem">Scaling up</Heading>
+            <p>Scaling up Signalen at Dutch municipalities.</p>
+            <ResponsiveImage src={doorontwikkeling} />
           </Timeline.Content>
         </Timeline.Container>
+
+        <Timeline.Container align="left">
+          <Timeline.Content>
+            <Span fontSize="0.9rem">Q4 2020</Span>
+            <Heading as="h3" fontSize="1.5rem">Ready for production</Heading>
+            <p>Delivery of a municipal neutral version of the Signalen software. Signalen is ready for production for the municipal participants from the core group.</p>
+          </Timeline.Content>
+        </Timeline.Container>
+
+        <Timeline.Container align="right">
+          <Timeline.Content>
+            <Span fontSize="0.9rem">Sept 2020</Span>
+            <Heading as="h3" fontSize="1.5rem">Integral roadmap</Heading>
+            <p>The start towards one integral functional roadmap. The roadmap shows where Signalen is going and contains features that are valuable for every municipality.</p>
+          </Timeline.Content>
+        </Timeline.Container>
+
+        <Timeline.Container align="left">
+          <Timeline.Content>
+            <Span fontSize="0.9rem">Sept 2020</Span>
+            <Heading as="h3" fontSize="1.5rem">Signalen as a Service</Heading>
+            <p>Elaboration of the municipal requirements and wishes for management and support on 'Signalen-as-a-Service'.</p>
+            <ResponsiveImage src={saas} />
+          </Timeline.Content>
+        </Timeline.Container>
+
+        <Timeline.Container align="right">
+          <Timeline.Content>
+            <Span fontSize="0.9rem">Aug 2020</Span>
+            <Heading as="h3" fontSize="1.5rem">First community day</Heading>
+            <p>First Signalen Community day with the core group. The municipalities of The Hague, Utrecht and Haarlem are also present as Signalen supporters.</p>
+            <ResponsiveImage src={community} />
+          </Timeline.Content>
+        </Timeline.Container>
+
         <Timeline.Container align="left">
           <Timeline.Content>
             <Span fontSize="0.9rem">April 2020</Span>
@@ -75,44 +103,35 @@ const RoadmapPage = ({ location }) => (
             <ResponsiveImage src={sprint} />
           </Timeline.Content>
         </Timeline.Container>
+
         <Timeline.Container align="right">
           <Timeline.Content>
-            <Span fontSize="0.9rem">Aug 2020</Span>
-            <Heading as="h3" fontSize="1.5rem">First community day</Heading>
-            <p>First Signalen Community day with the core group. The municipalities of The Hague, Utrecht and Haarlem are also present as Signalen supporters.</p>
-            <ResponsiveImage src={community} />
+            <Span fontSize="0.9rem">March 2nd, 2020</Span>
+            <Heading as="h3" fontSize="1.5rem">Signalen core group</Heading>
+            <p>'s-Hertogenbosch, Amsterdam, Almere, VNG Realization and the Foundation for Public Code together form the Kopgroep with the aim of making Signalen usable for other municipalities.</p>
+            <ResponsiveImage src={connectingTeams} />
           </Timeline.Content>
         </Timeline.Container>
+
         <Timeline.Container align="left">
           <Timeline.Content>
-            <Span fontSize="0.9rem">Sept 2020</Span>
-            <Heading as="h3" fontSize="1.5rem">Signalen as a Service</Heading>
-            <p>Elaboration of the municipal requirements and wishes for management and support on 'Signalen-as-a-Service'.</p>
-            <ResponsiveImage src={saas} />
+            <Span fontSize="0.9rem">2020</Span>
+            <Heading as="h3" fontSize="1.5rem">Start initiative</Heading>
+            <p>Start of the Signalen initiative! Many municipalities in the Netherlands are interested in Signalen. Based on this enthusiasm, VNGr starts scaling up Signalen.</p>
+            <ResponsiveImage src={startInitiatief} />
           </Timeline.Content>
         </Timeline.Container>
+
         <Timeline.Container align="right">
-          <Timeline.Content>
-            <Span fontSize="0.9rem">Sept 2020</Span>
-            <Heading as="h3" fontSize="1.5rem">Integral roadmap</Heading>
-            <p>The start towards one integral functional roadmap. The roadmap shows where Signalen is going and contains features that are valuable for every municipality.</p>
-          </Timeline.Content>
-        </Timeline.Container>
-        <Timeline.Container align="left">
-          <Timeline.Content>
-            <Span fontSize="0.9rem">Q4 2020</Span>
-            <Heading as="h3" fontSize="1.5rem">Ready for production</Heading>
-            <p>Delivery of a municipal neutral version of the Signalen software. Signalen is ready for production for the municipal participants from the core group.</p>
-          </Timeline.Content>
-        </Timeline.Container>
-        <Timeline.Container align="right">
-          <Timeline.Content>
-            <Span fontSize="0.9rem">2021</Span>
-            <Heading as="h3" fontSize="1.5rem">Scaling up</Heading>
-            <p>Scaling up Signalen at Dutch municipalities.</p>
-            <ResponsiveImage src={doorontwikkeling} />
-          </Timeline.Content>
-        </Timeline.Container>
+            <Timeline.Content>
+              <Span fontSize="0.9rem">2018</Span>
+              <Heading as="h3" fontSize="1.5rem">Start of development of SIA</Heading>
+              <p>The Municipality of Amsterdam starts the development of Signalen in Amsterdam (SIA) with the Department of Services and the Datalab. SIA is open source and therefore freely usable for others.</p>
+              <ResponsiveImage src={startOntwikkeling} />
+            </Timeline.Content>
+          </Timeline.Container>
+
+
       </Timeline>
     </Container>
     <Footer location={location} />
