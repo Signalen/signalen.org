bd369b5f31acc975e53f3c91095df245dba7f434
jaccobrouwer <63238094+jaccobrouwer@users.noreply.github.com>
2022-01-05 12:08:21 +0100

    Update index.js

--- src/components/Logos/index.js
+++ src/components/Logos/index.js
@@ -4,6 +4,7 @@ import { StyledContainer } from './index.styles.js'
 import gemeenteAmsterdam from './images/gemeente-amsterdam.png'
 import gemeenteAlmere from './images/gemeente-almere.png'
 import gemeenteDenBosch from './images/gemeente-den-bosch.png'
+import gemeenteUtrecht from './images/gemeente-utecht.png'
 import vng from './images/vng.png'
 import foundationForPublicCode from './images/foundation-for-public-code.png'
 
@@ -12,6 +13,7 @@ const Logos = () => (
     <a href="https://www.amsterdam.nl/" target="_blank" rel="noopener noreferrer"><img src={gemeenteAmsterdam} height="40" alt="Logo Gemeente Amsterdam" /></a>
     <a href="http://almere.nl/" target="_blank" rel="noopener noreferrer"><img src={gemeenteAlmere} height="45" alt="Logo Gemeente Almere" /></a>
     <a href="https://www.s-hertogenbosch.nl/" target="_blank" rel="noopener noreferrer"><img src={gemeenteDenBosch} height="40" alt="Logo Gemeente Den Bosch" /></a>
+    <a href="https://www.utrecht.nl/" target="_blank" rel="noopener noreferrer"><img src={gemeenteUtrecht} height="40" alt="Logo Gemeente Utrecht" /></a>
     <a href="https://www.vng.nl" target="_blank" rel="noopener noreferrer"><img src={vng} height="40" alt="Logo Vereniging van Nederlandse Gemeenten (VNG)" /></a>
     <a href="https://publiccode.net/" target="_blank" rel="noopener noreferrer"><img src={foundationForPublicCode} height="45" alt="Logo Foundation for Public Code" /></a>
   </StyledContainer>
